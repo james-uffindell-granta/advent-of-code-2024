c6a141193dbfd545f1a7f060e4feb72c7969591f
James Uffindell-Birt <james.uffindell-birt@ansys.com>
2024-12-19 09:30:02 +0000

    Days 18 and 19

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,3 +1,148 @@
+use std::collections::{HashSet, HashMap, VecDeque};
+use std::ops::Add;
+
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Coord {
+    x: i64,
+    y: i64,
+}
+
+impl From<(i64, i64)> for Coord {
+    fn from((x, y): (i64, i64)) -> Self {
+        Self { x, y }
+    }
+}
+
+impl Add<(i64, i64)> for Coord {
+    type Output = Coord;
+
+    fn add(self, (x, y): (i64, i64)) -> Self::Output {
+        Self::Output {
+            x: self.x + x,
+            y: self.y + y,
+        }
+    }
+}
+
+pub fn bfs(unreachable: &HashSet<Coord>, bounds: Coord) -> HashMap<Coord, Coord> {
+    let start = (0, 0).into();
+    let mut queue: VecDeque<Coord> = VecDeque::from([start]);
+    let mut scores = HashMap::new();
+    scores.insert(start, 0);
+    let mut predecessors = HashMap::new();
+
+    // do the bfs
+    while let Some(next) = queue.pop_front() {
+        let neighbours = [next + (0,1), next + (0, -1), next + (1, 0), next + (-1, 0)]
+            .into_iter().filter(|n| !unreachable.contains(n) && n.x >= 0 && n.x <= bounds.x && n.y >= 0 && n.y <= bounds.y);
+
+        for n in neighbours {
+            if !scores.contains_key(&n) {
+                scores.insert(n, scores.get(&next).unwrap() + 1);
+                predecessors.insert(n, next);
+                queue.push_back(n);
+            }
+        }
+        
+        // todo: do we want to break out if we hit the end?
+    }
+
+    predecessors
+}
+
+pub fn part_1(blocks: &[Coord], bounds: Coord, limit: usize) -> usize {
+    let unreachable = blocks.iter().copied().take(limit).collect::<HashSet<_>>();
+
+    let predecessors = bfs(&unreachable, bounds);
+
+    let mut steps_on_path = HashSet::new();
+    let mut current_cell = bounds;
+    // steps_on_path.insert(current_cell);
+    while let Some(pre) = predecessors.get(&current_cell) {
+        steps_on_path.insert(*pre);
+        current_cell = *pre;
+    }
+
+    // steps_on_path.insert(start);
+
+    // for y in 0..=bounds.y {
+    //     for x in 0..=bounds.x {
+    //         let c: Coord = (x, y).into();
+    //         if cells_on_path.contains(&c) {
+    //             print!("O");
+    //         } else if unreachable.contains(&c) {
+    //             print!("#");
+    //         } else {
+    //             print!(".");
+    //         }
+    //     }
+    //     println!();
+    // }
+    steps_on_path.len()
+}
+
+pub fn part_2(blocks: &[Coord], bounds: Coord, limit: usize) -> Coord {
+    let mut unreachable = blocks.iter().copied().take(limit).collect::<HashSet<_>>();
+
+    for &block in &blocks[limit..] {
+        unreachable.insert(block);
+
+        let predecessors = bfs(&unreachable, bounds);
+        match predecessors.get(&bounds) {
+            Some(_) => continue,
+            None => return block
+        }
+
+    }
+
+    unreachable!();
+}
+
+pub fn parse_input(input: &str) -> Vec<Coord> {
+    input.lines().map(|line| {
+        let (x, y) = line.trim().split_once(",").unwrap();
+        (x.parse().unwrap(), y.parse().unwrap()).into()
+    }).collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file = include_str!("../input.txt");
+    let input = parse_input(file.trim());
+    println!("Part 1: {}", part_1(&input, (70, 70).into(), 1024));
+    println!("Part 2: {:?}", part_2(&input, (70, 70).into(), 1024));
+}
+
+#[test]
+pub fn test() {
+    let input = "5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0
+";
+    let input = parse_input(input);
+
+    assert_eq!(part_1(&input, (6, 6).into(), 12), 22);
+    assert_eq!(part_2(&input, (6, 6).into(), 12), Coord { x: 6, y: 1 });
+    // no clue how to test part 2 here
 }
--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,3 +1,143 @@
+use std::collections::HashMap;
+
+
+#[derive(Clone, Debug, Hash)]
+pub struct Input {
+    towels: Vec<String>,
+    designs: Vec<String>,
+}
+
+impl Input {
+
+}
+
+pub fn parse_input(input: &str) -> Input {
+    let (towels_part, designs_part) = input.split_once("\n\n").unwrap();
+
+    let towels = towels_part.split(",").map(|s| s.trim().to_string()).collect();
+    let designs = designs_part.trim().lines().map(|line| line.to_string()).collect();
+
+    Input { towels, designs }
+}
+
+pub fn can_make(design: &str, towels_by_char: &HashMap<u8, Vec<String>>, cache: &mut HashMap<String, bool>) -> bool {
+    if let Some(known_result)  = cache.get(design) {
+        return *known_result;
+    }
+
+    if design.is_empty() {
+        return true;
+    }
+
+    // otherwise see what we can do
+    let first_byte = design.as_bytes()[0];
+    if let Some(possible_towels) = towels_by_char.get(&first_byte) {
+        for t in possible_towels {
+            if design.starts_with(t) {
+                let rest = &design[t.len()..];
+                let answer_rest = can_make(rest, towels_by_char, cache);
+                if answer_rest {
+                    // we can make it starting with this towel
+                    cache.insert(design.to_string(), true);
+                    return true;
+                }
+
+                // otherwise we can't - try the next one
+            }
+        }
+    }
+
+    cache.insert(design.to_string(), false);
+    false
+}
+
+pub fn ways_to_make(design: &str, towels_by_char: &HashMap<u8, Vec<String>>, cache: &mut HashMap<String, usize>) -> usize {
+    if let Some(known_result)  = cache.get(design) {
+        return *known_result;
+    }
+
+    if design.is_empty() {
+        return 1;
+    }
+
+    // otherwise see what we can do
+    let mut ways = 0;
+    let first_byte = design.as_bytes()[0];
+    if let Some(possible_towels) = towels_by_char.get(&first_byte) {
+        for t in possible_towels {
+            if design.starts_with(t) {
+                let rest = &design[t.len()..];
+                let answer_rest = ways_to_make(rest, towels_by_char, cache);
+                if answer_rest > 0 {
+                    // we can make it starting with this towel
+                    ways += answer_rest
+                }
+
+                // otherwise we can't - try the next one
+            }
+        }
+    }
+
+    cache.insert(design.to_string(), ways);
+    ways
+}
+
+pub fn part_1(input: &Input) -> usize {
+    let mut towels_by_char = HashMap::new();
+    for towel in &input.towels {
+        towels_by_char.entry(towel.as_bytes()[0]).or_insert(Vec::new()).push(towel.clone());
+    }
+
+    let mut cache = HashMap::new();
+
+    let mut result = 0;
+    for design in &input.designs {
+        if can_make(&design, &towels_by_char, &mut cache) {
+            result += 1;
+        }
+    }
+
+    result
+}
+
+pub fn part_2(input: &Input) -> usize {
+    let mut towels_by_char = HashMap::new();
+    for towel in &input.towels {
+        towels_by_char.entry(towel.as_bytes()[0]).or_insert(Vec::new()).push(towel.clone());
+    }
+
+    let mut cache = HashMap::new();
+
+    let mut result = 0;
+    for design in &input.designs {
+        result += ways_to_make(design, &towels_by_char, &mut cache)
+    }
+
+    result
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file = include_str!("../input.txt");
+    let input = parse_input(file.trim());
+    println!("Part 1: {}", part_1(&input));
+    println!("Part 2: {:?}", part_2(&input));
+}
+
+#[test]
+pub fn test() {
+    let input = "r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb
+";
+    let input = parse_input(input);
+
+    assert_eq!(part_1(&input), 6);
+    assert_eq!(part_2(&input), 16);
 }
