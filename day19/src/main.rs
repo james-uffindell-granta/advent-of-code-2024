e23d7ad782b721a66b6fc51519399c671ab1e359
James Uffindell-Birt <james.uffindell-birt@ansys.com>
2024-12-19 10:01:31 +0000

    Simplify day 19 a bit - slightly slower but oh well
    60ms is still fast enough

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -2,26 +2,26 @@ use std::collections::HashMap;
 
 #[derive(Clone, Debug)]
 pub struct Input {
-    towels: HashMap<u8, Vec<String>>,
+    towels: Vec<String>,
     designs: Vec<String>,
 }
 
 pub fn parse_input(input: &str) -> Input {
     let (towels_part, designs_part) = input.split_once("\n\n").unwrap();
-
-    let mut towels = HashMap::new();
-    for towel in towels_part.split(",") {
-        let towel = towel.trim();
-        towels.entry(towel.as_bytes()[0]).or_insert(Vec::new()).push(towel.to_string());
-    }
-
-    let designs = designs_part.trim().lines().map(|line| line.to_string()).collect();
-
+    let towels = towels_part
+        .split(",")
+        .map(|t| t.trim().to_string())
+        .collect();
+    let designs = designs_part
+        .trim()
+        .lines()
+        .map(|line| line.to_string())
+        .collect();
     Input { towels, designs }
 }
 
-pub fn ways_to_make(design: &str, towels_by_char: &HashMap<u8, Vec<String>>, cache: &mut HashMap<String, usize>) -> usize {
-    if let Some(known_result)  = cache.get(design) {
+pub fn ways_to_make(design: &str, towels: &[String], cache: &mut HashMap<String, usize>) -> usize {
+    if let Some(known_result) = cache.get(design) {
         return *known_result;
     }
 
@@ -29,48 +29,38 @@ pub fn ways_to_make(design: &str, towels_by_char: &HashMap<u8, Vec<String>>, cac
         return 1;
     }
 
-    // otherwise see what we can do
-    let mut ways = 0;
-    let first_byte = design.as_bytes()[0];
-    if let Some(possible_towels) = towels_by_char.get(&first_byte) {
-        for t in possible_towels {
-            if design.starts_with(t) {
-                let rest = &design[t.len()..];
-                let answer_rest = ways_to_make(rest, towels_by_char, cache);
-                if answer_rest > 0 {
-                    // we can make it starting with this towel
-                    ways += answer_rest
-                }
-
-                // otherwise we can't - try the next one
-            }
-        }
-    }
-
+    let ways = towels
+        .iter()
+        .filter(|&t| design.starts_with(t))
+        .map(|t| ways_to_make(&design[t.len()..], towels, cache))
+        .sum();
     cache.insert(design.to_string(), ways);
     ways
 }
 
 pub fn part_1(input: &Input) -> usize {
     let mut cache = HashMap::new();
-    input.designs.iter().filter(|d| ways_to_make(d, &input.towels, &mut cache) > 0).count()
+    input
+        .designs
+        .iter()
+        .filter(|d| ways_to_make(d, &input.towels, &mut cache) > 0)
+        .count()
 }
 
 pub fn part_2(input: &Input) -> usize {
     let mut cache = HashMap::new();
-    input.designs.iter().map(|d| ways_to_make(d, &input.towels, &mut cache)).sum()
+    input
+        .designs
+        .iter()
+        .map(|d| ways_to_make(d, &input.towels, &mut cache))
+        .sum()
 }
 
 fn main() {
     let file = include_str!("../input.txt");
     let input = parse_input(file.trim());
-    use std::time::Instant;
-    let now = Instant::now();
     println!("Part 1: {}", part_1(&input));
-    println!("{:?}", now.elapsed());
-    let now = Instant::now();
     println!("Part 2: {}", part_2(&input));
-    println!("{:?}", now.elapsed());
 }
 
 #[test]
