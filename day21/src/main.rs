2a3949b5d76346af01f98bdc3467ae335b79ce1a
James Uffindell-Birt <james.uffindell-birt@ansys.com>
2024-12-21 10:44:02 +0000

    Fix formatting

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -27,19 +27,44 @@ pub fn horizontal_between(start: KeypadButton, target: KeypadButton) -> Option<(
     match start {
         KeypadButton::One | KeypadButton::Four | KeypadButton::Seven => match target {
             KeypadButton::One | KeypadButton::Four | KeypadButton::Seven => None,
-            KeypadButton::Zero | KeypadButton::Two | KeypadButton::Five | KeypadButton::Eight => Some((Direction::Right, 1)),
-            KeypadButton::Three | KeypadButton::Six | KeypadButton::Nine | KeypadButton::Activate=> Some((Direction::Right, 2)),
+            KeypadButton::Zero | KeypadButton::Two | KeypadButton::Five | KeypadButton::Eight => {
+                Some((Direction::Right, 1))
+            }
+            KeypadButton::Three
+            | KeypadButton::Six
+            | KeypadButton::Nine
+            | KeypadButton::Activate => Some((Direction::Right, 2)),
         },
-        KeypadButton::Two | KeypadButton::Five | KeypadButton::Eight | KeypadButton::Zero => match target {
-            KeypadButton::One | KeypadButton::Four | KeypadButton::Seven => Some((Direction::Left, 1)),
-            KeypadButton::Zero | KeypadButton::Two | KeypadButton::Five | KeypadButton::Eight => None,
-            KeypadButton::Three | KeypadButton::Six | KeypadButton::Nine | KeypadButton::Activate=> Some((Direction::Right, 1)),
-        },
-        KeypadButton::Three | KeypadButton::Six | KeypadButton::Nine | KeypadButton::Activate => match target {
-            KeypadButton::One | KeypadButton::Four | KeypadButton::Seven => Some((Direction::Left, 2)),
-            KeypadButton::Zero | KeypadButton::Two | KeypadButton::Five | KeypadButton::Eight => Some((Direction::Left, 1)),
-            KeypadButton::Three | KeypadButton::Six | KeypadButton::Nine | KeypadButton::Activate=> None,
-        },
+        KeypadButton::Two | KeypadButton::Five | KeypadButton::Eight | KeypadButton::Zero => {
+            match target {
+                KeypadButton::One | KeypadButton::Four | KeypadButton::Seven => {
+                    Some((Direction::Left, 1))
+                }
+                KeypadButton::Zero
+                | KeypadButton::Two
+                | KeypadButton::Five
+                | KeypadButton::Eight => None,
+                KeypadButton::Three
+                | KeypadButton::Six
+                | KeypadButton::Nine
+                | KeypadButton::Activate => Some((Direction::Right, 1)),
+            }
+        }
+        KeypadButton::Three | KeypadButton::Six | KeypadButton::Nine | KeypadButton::Activate => {
+            match target {
+                KeypadButton::One | KeypadButton::Four | KeypadButton::Seven => {
+                    Some((Direction::Left, 2))
+                }
+                KeypadButton::Zero
+                | KeypadButton::Two
+                | KeypadButton::Five
+                | KeypadButton::Eight => Some((Direction::Left, 1)),
+                KeypadButton::Three
+                | KeypadButton::Six
+                | KeypadButton::Nine
+                | KeypadButton::Activate => None,
+            }
+        }
     }
 }
 
@@ -49,24 +74,36 @@ pub fn vertical_between(start: KeypadButton, target: KeypadButton) -> Option<(Di
             KeypadButton::Zero | KeypadButton::Activate => None,
             KeypadButton::One | KeypadButton::Two | KeypadButton::Three => Some((Direction::Up, 1)),
             KeypadButton::Four | KeypadButton::Five | KeypadButton::Six => Some((Direction::Up, 2)),
-            KeypadButton::Seven | KeypadButton::Eight | KeypadButton::Nine => Some((Direction::Up, 3)),
+            KeypadButton::Seven | KeypadButton::Eight | KeypadButton::Nine => {
+                Some((Direction::Up, 3))
+            }
         },
         KeypadButton::One | KeypadButton::Two | KeypadButton::Three => match target {
             KeypadButton::Zero | KeypadButton::Activate => Some((Direction::Down, 1)),
             KeypadButton::One | KeypadButton::Two | KeypadButton::Three => None,
             KeypadButton::Four | KeypadButton::Five | KeypadButton::Six => Some((Direction::Up, 1)),
-            KeypadButton::Seven | KeypadButton::Eight | KeypadButton::Nine => Some((Direction::Up, 2)),
+            KeypadButton::Seven | KeypadButton::Eight | KeypadButton::Nine => {
+                Some((Direction::Up, 2))
+            }
         },
         KeypadButton::Four | KeypadButton::Five | KeypadButton::Six => match target {
             KeypadButton::Zero | KeypadButton::Activate => Some((Direction::Down, 2)),
-            KeypadButton::One | KeypadButton::Two | KeypadButton::Three => Some((Direction::Down, 1)),
+            KeypadButton::One | KeypadButton::Two | KeypadButton::Three => {
+                Some((Direction::Down, 1))
+            }
             KeypadButton::Four | KeypadButton::Five | KeypadButton::Six => None,
-            KeypadButton::Seven | KeypadButton::Eight | KeypadButton::Nine => Some((Direction::Up, 1)),
+            KeypadButton::Seven | KeypadButton::Eight | KeypadButton::Nine => {
+                Some((Direction::Up, 1))
+            }
         },
         KeypadButton::Seven | KeypadButton::Eight | KeypadButton::Nine => match target {
             KeypadButton::Zero | KeypadButton::Activate => Some((Direction::Down, 3)),
-            KeypadButton::One | KeypadButton::Two | KeypadButton::Three => Some((Direction::Down, 2)),
-            KeypadButton::Four | KeypadButton::Five | KeypadButton::Six => Some((Direction::Down, 1)),
+            KeypadButton::One | KeypadButton::Two | KeypadButton::Three => {
+                Some((Direction::Down, 2))
+            }
+            KeypadButton::Four | KeypadButton::Five | KeypadButton::Six => {
+                Some((Direction::Down, 1))
+            }
             KeypadButton::Seven | KeypadButton::Eight | KeypadButton::Nine => None,
         },
     }
@@ -81,7 +118,10 @@ pub enum DirectionalButton {
     Activate,
 }
 
-pub fn shortest_sequence_to_press(start: DirectionalButton, target: DirectionalButton) -> Vec<DirectionalButton> {
+pub fn shortest_sequence_to_press(
+    start: DirectionalButton,
+    target: DirectionalButton,
+) -> Vec<DirectionalButton> {
     // go from start to target and press it
     // assumes we want the first robot is on button start and needs to move to target and press it
     // assumes the next robot is on A
@@ -89,43 +129,92 @@ pub fn shortest_sequence_to_press(start: DirectionalButton, target: DirectionalB
     match start {
         DirectionalButton::Up => match target {
             DirectionalButton::Up => vec![DirectionalButton::Activate],
-            DirectionalButton::Left => vec![DirectionalButton::Down, DirectionalButton::Left, DirectionalButton::Activate],
-            DirectionalButton::Right => vec![DirectionalButton::Down, DirectionalButton::Right, DirectionalButton::Activate],
+            DirectionalButton::Left => vec![
+                DirectionalButton::Down,
+                DirectionalButton::Left,
+                DirectionalButton::Activate,
+            ],
+            DirectionalButton::Right => vec![
+                DirectionalButton::Down,
+                DirectionalButton::Right,
+                DirectionalButton::Activate,
+            ],
             DirectionalButton::Down => vec![DirectionalButton::Down, DirectionalButton::Activate],
-            DirectionalButton::Activate => vec![DirectionalButton::Right, DirectionalButton::Activate],
+            DirectionalButton::Activate => {
+                vec![DirectionalButton::Right, DirectionalButton::Activate]
+            }
         },
         DirectionalButton::Left => match target {
-            DirectionalButton::Up => vec![DirectionalButton::Right, DirectionalButton::Up, DirectionalButton::Activate],
+            DirectionalButton::Up => vec![
+                DirectionalButton::Right,
+                DirectionalButton::Up,
+                DirectionalButton::Activate,
+            ],
             DirectionalButton::Left => vec![DirectionalButton::Activate],
-            DirectionalButton::Right => vec![DirectionalButton::Right, DirectionalButton::Right, DirectionalButton::Activate],
+            DirectionalButton::Right => vec![
+                DirectionalButton::Right,
+                DirectionalButton::Right,
+                DirectionalButton::Activate,
+            ],
             DirectionalButton::Down => vec![DirectionalButton::Right, DirectionalButton::Activate],
-            DirectionalButton::Activate => vec![DirectionalButton::Right, DirectionalButton::Right, DirectionalButton::Up, DirectionalButton::Activate],
+            DirectionalButton::Activate => vec![
+                DirectionalButton::Right,
+                DirectionalButton::Right,
+                DirectionalButton::Up,
+                DirectionalButton::Activate,
+            ],
         },
         DirectionalButton::Right => match target {
-            DirectionalButton::Up => vec![DirectionalButton::Left, DirectionalButton::Up, DirectionalButton::Activate],
-            DirectionalButton::Left => vec![DirectionalButton::Left, DirectionalButton::Left, DirectionalButton::Activate],
+            DirectionalButton::Up => vec![
+                DirectionalButton::Left,
+                DirectionalButton::Up,
+                DirectionalButton::Activate,
+            ],
+            DirectionalButton::Left => vec![
+                DirectionalButton::Left,
+                DirectionalButton::Left,
+                DirectionalButton::Activate,
+            ],
             DirectionalButton::Right => vec![DirectionalButton::Activate],
             DirectionalButton::Down => vec![DirectionalButton::Left, DirectionalButton::Activate],
             DirectionalButton::Activate => vec![DirectionalButton::Up, DirectionalButton::Activate],
-        }
+        },
         DirectionalButton::Down => match target {
             DirectionalButton::Up => vec![DirectionalButton::Up, DirectionalButton::Activate],
             DirectionalButton::Left => vec![DirectionalButton::Left, DirectionalButton::Activate],
             DirectionalButton::Right => vec![DirectionalButton::Right, DirectionalButton::Activate],
             DirectionalButton::Down => vec![DirectionalButton::Activate],
-            DirectionalButton::Activate => vec![DirectionalButton::Up, DirectionalButton::Right, DirectionalButton::Activate],
+            DirectionalButton::Activate => vec![
+                DirectionalButton::Up,
+                DirectionalButton::Right,
+                DirectionalButton::Activate,
+            ],
         },
         DirectionalButton::Activate => match target {
             DirectionalButton::Up => vec![DirectionalButton::Left, DirectionalButton::Activate],
-            DirectionalButton::Left => vec![DirectionalButton::Down, DirectionalButton::Left, DirectionalButton::Left, DirectionalButton::Activate],
+            DirectionalButton::Left => vec![
+                DirectionalButton::Down,
+                DirectionalButton::Left,
+                DirectionalButton::Left,
+                DirectionalButton::Activate,
+            ],
             DirectionalButton::Right => vec![DirectionalButton::Down, DirectionalButton::Activate],
-            DirectionalButton::Down => vec![DirectionalButton::Left, DirectionalButton::Down, DirectionalButton::Activate],
+            DirectionalButton::Down => vec![
+                DirectionalButton::Left,
+                DirectionalButton::Down,
+                DirectionalButton::Activate,
+            ],
             DirectionalButton::Activate => vec![DirectionalButton::Activate],
-        }
+        },
     }
 }
 
-pub fn shortest_sequence_length_between(start: KeypadButton, target: KeypadButton, intervening_robots: usize, cache: &mut HashMap<(DirectionalButton, DirectionalButton, usize), usize>) -> usize {
+pub fn shortest_sequence_length_between(
+    start: KeypadButton,
+    target: KeypadButton,
+    intervening_robots: usize,
+    cache: &mut HashMap<(DirectionalButton, DirectionalButton, usize), usize>,
+) -> usize {
     // we start at start, we go to target
     let horizontal_moves = horizontal_between(start, target);
     let vertical_moves = vertical_between(start, target);
@@ -145,7 +234,10 @@ pub fn shortest_sequence_length_between(start: KeypadButton, target: KeypadButto
     }
 
     if let Some((Direction::Down, times)) = vertical_moves {
-        if start == KeypadButton::One || (start == KeypadButton::Four && times == 2) || (start == KeypadButton::Seven && times == 3) {
+        if start == KeypadButton::One
+            || (start == KeypadButton::Four && times == 2)
+            || (start == KeypadButton::Seven && times == 3)
+        {
             postponed_down = true;
         } else {
             first_robot_presses.extend(std::iter::repeat(DirectionalButton::Down).take(times));
@@ -154,7 +246,7 @@ pub fn shortest_sequence_length_between(start: KeypadButton, target: KeypadButto
 
     if let Some((Direction::Up, times)) = vertical_moves {
         first_robot_presses.extend(std::iter::repeat(DirectionalButton::Up).take(times));
-    } 
+    }
 
     if let Some((Direction::Right, times)) = horizontal_moves {
         first_robot_presses.extend(std::iter::repeat(DirectionalButton::Right).take(times));
@@ -163,13 +255,13 @@ pub fn shortest_sequence_length_between(start: KeypadButton, target: KeypadButto
     if postponed_left {
         if let Some((Direction::Left, times)) = horizontal_moves {
             first_robot_presses.extend(std::iter::repeat(DirectionalButton::Left).take(times));
-        }    
+        }
     }
 
     if postponed_down {
         if let Some((Direction::Down, times)) = vertical_moves {
             first_robot_presses.extend(std::iter::repeat(DirectionalButton::Down).take(times));
-        }  
+        }
     }
 
     first_robot_presses.push(DirectionalButton::Activate);
@@ -184,7 +276,12 @@ pub fn shortest_sequence_length_between(start: KeypadButton, target: KeypadButto
 }
 
 // shortest sequence for the next n robots to tell me to press this button from there
-pub fn all_robot_presses(start: DirectionalButton, target: DirectionalButton, remaining_robots: usize, cache: &mut HashMap<(DirectionalButton, DirectionalButton, usize), usize>) -> usize {
+pub fn all_robot_presses(
+    start: DirectionalButton,
+    target: DirectionalButton,
+    remaining_robots: usize,
+    cache: &mut HashMap<(DirectionalButton, DirectionalButton, usize), usize>,
+) -> usize {
     if let Some(answer) = cache.get(&(start, target, remaining_robots)) {
         return *answer;
     }
@@ -218,7 +315,8 @@ impl Sequence {
         numbers.insert(0, KeypadButton::Activate);
         let mut presses = 0;
         for pair in numbers.windows(2) {
-            presses += (shortest_sequence_length_between(pair[0], pair[1], intervening_robots, &mut cache));
+            presses +=
+                shortest_sequence_length_between(pair[0], pair[1], intervening_robots, &mut cache);
         }
 
         presses
@@ -232,29 +330,37 @@ impl Sequence {
 }
 
 pub struct Input {
-    codes: Vec<Sequence>
+    codes: Vec<Sequence>,
 }
 
 pub fn parse_input(input: &str) -> Input {
-    Input { codes: 
-        input.lines().map(|line| {
-            let numeric = line[..line.len() - 1].parse().unwrap();
-            Sequence { numbers: line.chars().map(|c| match c {
-                'A' => KeypadButton::Activate,
-                '1' => KeypadButton::One,
-                '2' => KeypadButton::Two,
-                '3' => KeypadButton::Three,
-                '4' => KeypadButton::Four,
-                '5' => KeypadButton::Five,
-                '6' => KeypadButton::Six,
-                '7' => KeypadButton::Seven,
-                '8' => KeypadButton::Eight,
-                '9' => KeypadButton::Nine,
-                '0' => KeypadButton::Zero,
-                _ => unreachable!(),
-            }).collect(),
-            numeric_part: numeric }
-        }).collect()
+    Input {
+        codes: input
+            .lines()
+            .map(|line| {
+                let numeric = line[..line.len() - 1].parse().unwrap();
+                Sequence {
+                    numbers: line
+                        .chars()
+                        .map(|c| match c {
+                            'A' => KeypadButton::Activate,
+                            '1' => KeypadButton::One,
+                            '2' => KeypadButton::Two,
+                            '3' => KeypadButton::Three,
+                            '4' => KeypadButton::Four,
+                            '5' => KeypadButton::Five,
+                            '6' => KeypadButton::Six,
+                            '7' => KeypadButton::Seven,
+                            '8' => KeypadButton::Eight,
+                            '9' => KeypadButton::Nine,
+                            '0' => KeypadButton::Zero,
+                            _ => unreachable!(),
+                        })
+                        .collect(),
+                    numeric_part: numeric,
+                }
+            })
+            .collect(),
     }
 }
 
@@ -272,7 +378,7 @@ fn main() {
     use std::time::Instant;
     let now = Instant::now();
     println!("Part 1: {}", part_1(&input)); // 163280 - too high
-    // had lengths 78, 76, 68, 74, 74, 
+    // had lengths 78, 76, 68, 74, 74,
     println!("{:?}", now.elapsed());
     let now = Instant::now();
     println!("Part 2: {}", part_2(&input)); // 294235936545928 too high
